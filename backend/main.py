a38736797f64faa908cf1c835a6d736d88ed35f2
Ruben35 <ruben111416@gmail.com>
2023-05-03 14:35:01 -0600

    🥅 Added Error Handling Middleware

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
 from routers.notes_router import notes_router
 
 # Init of FastAPI
@@ -11,6 +12,9 @@ app.version = "0.0.1"
 
 # Creating SQLite Database
 Base.metadata.create_all(bind=engine)
+
+# Adding middlewares
+app.add_middleware(ErrorHandler)
 
 # Adding routers
 app.include_router(notes_router)
