672a389f88c61dcddf0a18ffa193a15f588f170e
Ruben35 <ruben111416@gmail.com>
2023-05-03 14:06:12 -0600

    🦺 Adding NoteModel validation

--- backend/models/note.py
+++ backend/models/note.py
@@ -1,5 +1,7 @@
 from config.database import Base
 from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class Note(Base):
@@ -7,3 +9,18 @@ class Note(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     description = Column(String(1000), nullable=False)
+
+
+class NoteModel(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=1000)
+
+    class Config:
+        scheme_extra = {
+            "example": {
+                "id": 1,
+                "title": "Nueva Nota",
+                "description": "Cuerpo de la Nota"
+            }
+        }
