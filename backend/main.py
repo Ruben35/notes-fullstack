a614bde68b614679e5f7217cdee8a54d778a79c0
Ruben35 <ruben111416@gmail.com>
2023-05-05 00:11:45 -0600

    ⚗️ Adding CORS All just for simplity

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
+from fastapi.middleware.cors import CORSMiddleware
 from routers.notes_router import notes_router
 from routers.user_router import user_router
 from routers.auth_router import auth_router
@@ -17,6 +18,13 @@ Base.metadata.create_all(bind=engine)
 
 # Adding middlewares
 app.add_middleware(ErrorHandler)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Adding routers
 app.include_router(notes_router)
